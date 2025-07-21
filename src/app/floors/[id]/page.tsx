d3453190fffb65f0ce488400cf5d2a57318a015f
Γιώργος Παγώνης <georgios.pagonis@gmail.com>
2025-07-21 22:50:37 +0000

    Οκ, το αρχείο Paper3.pdf υπάρχει σωστά στον φάκελο floor-plans.
    Τώρα:

--- src/app/floors/[id]/page.tsx
+++ src/app/floors/[id]/page.tsx
@@ -311,6 +311,8 @@ export default function FloorDetailsPage() {
       await uploadBytes(fileRef, selectedFile, metadata);
       const url = await getDownloadURL(fileRef);
 
+      console.log('File available at', url);
+
       const floorDocRef = doc(db, 'floors', floor.id);
       await updateDoc(floorDocRef, { floorPlanUrl: url });
 
@@ -628,5 +630,7 @@ export default function FloorDetailsPage() {
     </div>
   );
 }
+
+    
 
     
