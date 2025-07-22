34fd1ad54c9c44928c089caefc1cb5e7330fb1bd
Γιώργος Παγώνης <georgios.pagonis@gmail.com>
2025-07-22 13:58:07 +0000

    Βλέπω ότι το σφάλμα CORS παραμένει:
    vbnet
    Αντιγραφή
    has been blocked by

--- src/app/floors/[id]/page.tsx
+++ src/app/floors/[id]/page.tsx
@@ -310,7 +310,7 @@ export default function FloorDetailsPage() {
       };
       await uploadBytes(fileRef, selectedFile, metadata);
       const url = await getDownloadURL(fileRef);
-      console.log('File available at', url);
+      console.log('File available at:', url);
 
       const floorDocRef = doc(db, 'floors', floor.id);
       await updateDoc(floorDocRef, { floorPlanUrl: url });
@@ -629,3 +629,5 @@ export default function FloorDetailsPage() {
     </div>
   );
 }
+
+    
