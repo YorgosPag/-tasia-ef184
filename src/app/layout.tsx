348314a7f88eb57b541ce277de2dc618cd755751
Γιώργος Παγώνης <georgios.pagonis@gmail.com>
2025-07-23 09:17:06 +0000

    Ενεργοποίησε dark mode με αυτόματη ανίχνευση συστήματος και toggle επιλο

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,7 @@ import { AuthProvider } from '@/hooks/use-auth';
 import { DataProvider } from '@/hooks/use-data-store';
 import { QueryProvider } from '@/hooks/use-query-provider';
 import { ProtectedRoute } from '@/components/auth/protected-route';
+import { ThemeProvider } from '@/components/theme-provider';
 
 export default function RootLayout({
   children,
@@ -27,24 +28,31 @@ export default function RootLayout({
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
       </head>
       <body className="font-body antialiased">
-        <AuthProvider>
-          <QueryProvider>
-            <DataProvider>
-              <ProtectedRoute>
-                <SidebarProvider defaultOpen>
-                  <AppSidebar />
-                  <div className="flex flex-1 flex-col">
-                    <AppHeader />
-                    <main className="flex-1 p-2 md:p-4">
-                      {children}
-                    </main>
-                  </div>
-                </SidebarProvider>
-              </ProtectedRoute>
-              <Toaster />
-            </DataProvider>
-          </QueryProvider>
-        </AuthProvider>
+        <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+        >
+          <AuthProvider>
+            <QueryProvider>
+              <DataProvider>
+                <ProtectedRoute>
+                  <SidebarProvider defaultOpen>
+                    <AppSidebar />
+                    <div className="flex flex-1 flex-col">
+                      <AppHeader />
+                      <main className="flex-1 p-2 md:p-4">
+                        {children}
+                      </main>
+                    </div>
+                  </SidebarProvider>
+                </ProtectedRoute>
+                <Toaster />
+              </DataProvider>
+            </QueryProvider>
+          </AuthProvider>
+        </ThemeProvider>
       </body>
     </html>
   );
