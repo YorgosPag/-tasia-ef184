dead99ad21dbc2d7123ee857af20f4433632b68c
Georgios Pagonis <georgios.pagonis@gmail.com>
2025-07-24 19:24:32 +0000

    Τίτλος Λίστας	Κωδικός Στοιχείου	Τιμή Στοιχείου Δημόσιες Οικονομικές Υπηρ

---
+++ src/app/eco/interventions-catalog/page.tsx
@@ -0,0 +1,11 @@
+
+"use client";
+
+export default function InterventionsCatalogPage() {
+    return (
+        <div>
+            <h1>Κατάλογος Παρεμβάσεων - Εξοικονομώ</h1>
+            <p>Αυτή είναι η σελίδα καταλόγου παρεμβάσεων για την εφαρμογή "Εξοικονομώ".</p>
+        </div>
+    );
+}
---
+++ src/app/eco/project-interventions/page.tsx
@@ -0,0 +1,11 @@
+
+"use client";
+
+export default function ProjectInterventionsPage() {
+    return (
+        <div>
+            <h1>Παρεμβάσεις Έργων - Εξοικονομώ</h1>
+            <p>Αυτή είναι η σελίδα παρεμβάσεων έργων για την εφαρμογή "Εξοικονομώ".</p>
+        </div>
+    );
+}
