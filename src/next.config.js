a0b06625d9ce70020397d8ccda214a688f9db0ab
Georgios Pagonis <georgios.pagonis@gmail.com>
2025-07-25 23:17:25 +0000

    The NextJS app server is having trouble starting. Please identify what c

---
+++ src/next.config.js
@@ -0,0 +1,37 @@
+/** @type {import('next').NextConfig} */
+import nextBundleAnalyzer from '@next/bundle-analyzer';
+
+const withBundleAnalyzer = nextBundleAnalyzer({
+  enabled: process***REMOVED***.ANALYZE === 'true',
+});
+
+const nextConfig = {
+  // Redirect / to /projects
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/projects',
+        permanent: true,
+      },
+    ];
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+export default withBundleAnalyzer(nextConfig);
