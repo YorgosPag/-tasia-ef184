efffd5b8f8bc1a162f1fad7133ead10766f1f263
Georgios Pagonis <georgios.pagonis@gmail.com>
2025-07-26 01:32:46 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/tasia/components/layout/app-shell.tsx
+++ src/tasia/components/layout/app-shell.tsx
@@ -3,13 +3,17 @@
 
 import { AppSidebar } from '@/shared/components/layout/Sidebar';
 import { AppShell as SharedAppShell } from '@/shared/components/layout/AppShell';
+import { AppHeader } from '@/shared/components/layout/Header';
 
 export function AppShell({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex-1 flex flex-col min-h-screen">
        <div className="flex flex-1">
           <AppSidebar />
-          <SharedAppShell>{children}</SharedAppShell>
+          <div className="flex-1 flex flex-col min-h-screen">
+            <AppHeader />
+            <main className="flex-1 p-4">{children}</main>
+          </div>
         </div>
     </div>
   );
