99ee051d3f18f86db17a3ba0223e7c46ca7e74e0
Georgios Pagonis <georgios.pagonis@gmail.com>
2025-07-26 19:26:32 +0000

    Θέλω η οθόνη να καταλαμβάνει το μέγιστο πλάτος

--- src/shared/components/layout/app-shell.tsx
+++ src/shared/components/layout/app-shell.tsx
@@ -1,4 +1,3 @@
-
 'use client';
 
 import React from 'react';
@@ -11,9 +10,9 @@ export function AppShell({ children }: { children: React.ReactNode }) {
   const domain = useCurrentDomain();
 
   return (
-    <div className={cn("flex min-h-screen", domain)}>
+    <div className={cn("flex min-h-screen w-full", domain)}>
       <AppSidebar />
-      <div className="flex-1 flex flex-col">
+      <div className="flex-1 flex flex-col w-full">
         <AppHeader />
         <main className="flex-1 p-4 sm:p-6 lg:p-8">
             {children}
