832b20a5495368d1ce284c884e42615ac1fcf5b4
Georgios Pagonis <georgios.pagonis@gmail.com>
2025-07-30 16:56:38 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,3 +1,4 @@
+
 // src/lib/firebase.ts
 import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getFirestore, enableIndexedDbPersistence } from 'firebase/firestore';
@@ -7,12 +8,12 @@ import { getFunctions } from 'firebase/functions';
 
 // Firebase configuration is loaded from environment variables
 const firebaseConfig = {
-  apiKey: process***REMOVED***.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process***REMOVED***.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process***REMOVED***.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process***REMOVED***.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process***REMOVED***.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process***REMOVED***.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "***REMOVED***",
+  authDomain: "tasia-6f771.firebaseapp.com",
+  projectId: "tasia-6f771",
+  storageBucket: "tasia-6f771.appspot.com",
+  messagingSenderId: "204877276202",
+  appId: "1:204877276202:web:31db4eb5b1c1b7c4078f53",
   measurementId: process***REMOVED***.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
