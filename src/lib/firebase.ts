d2eaff8019ffaa7e07f08a5f34697f73c006ba1a
Georgios Pagonis <georgios.pagonis@gmail.com>
2025-07-30 17:14:52 +0000

    διόρθωσέ το

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -8,12 +8,12 @@ import { getFunctions } from 'firebase/functions';
 
 // Firebase configuration is loaded from environment variables
 const firebaseConfig = {
-  apiKey: "***REMOVED***",
-  authDomain: "tasia-6f771.firebaseapp.com",
-  projectId: "tasia-6f771",
-  storageBucket: "tasia-6f771.appspot.com",
-  messagingSenderId: "204877276202",
-  appId: "1:204877276202:web:31db4eb5b1c1b7c4078f53",
+  apiKey: process***REMOVED***.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process***REMOVED***.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process***REMOVED***.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process***REMOVED***.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process***REMOVED***.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process***REMOVED***.NEXT_PUBLIC_FIREBASE_APP_ID,
   measurementId: process***REMOVED***.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
