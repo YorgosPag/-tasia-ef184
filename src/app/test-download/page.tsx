d551b6ff35ce0fc07b1e562f18dd23a46191d0f2
Georgios Pagonis <georgios.pagonis@gmail.com>
2025-07-30 09:46:53 +0000

    Πάμε χειρουργικά. Θα γράψουμε ένα μικρό React component που κάνει test τ

---
+++ src/app/test-download/page.tsx
@@ -0,0 +1,9 @@
+import { TestDownloadUrl } from '@/components/debug/TestDownloadUrl';
+
+export default function TestPage() {
+  return (
+    <div className="p-8">
+      <TestDownloadUrl />
+    </div>
+  );
+}
