e38d77facf99b500b9b96245c8f88a9c2df7f871
Georgios Pagonis <georgios.pagonis@gmail.com>
2025-07-30 18:28:06 +0000

    .json
    {
      "type": "service_account",
      "project_id": "tasia-6f77i",

---
+++ checkDoc.js
@@ -0,0 +1,35 @@
+import { initializeApp, getApps } from 'firebase/app';
+import { getFirestore, doc, getDoc } from 'firebase/firestore';
+
+// Firebase config (βάλε τα δικά σου ή import)
+const firebaseConfig = {
+  apiKey: "***REMOVED***",
+  authDomain: "tasia-6f771.firebaseapp.com",
+  projectId: "tasia-6f771",
+  storageBucket: "tasia-6f771.appspot.com",
+  messagingSenderId: "204877276202",
+  appId: "1:204877276202:web:31db4eb5b1c1b7c4078f53"
+};
+
+const app = getApps().length ? getApps()[0] : initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+const collectionName = 'tsia-complex-entities';
+const docId = '00ABWDQmABQWXP Aem5Ya'.replace(/\s/g, ''); // ΜΗΝ αφήσεις κενά
+
+async function checkDocument() {
+  try {
+    const docRef = doc(db, collectionName, docId);
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()) {
+      console.log('Document data:', docSnap.data());
+    } else {
+      console.log('No such document!');
+    }
+  } catch (error) {
+    console.error('Error fetching document:', error);
+  }
+}
+
+checkDocument();
---
+++ checkListId.cjs
@@ -0,0 +1,29 @@
+const admin = require('firebase-admin');
+const serviceAccount = require('./***REMOVED***');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  projectId: serviceAccount.project_id
+});
+
+const db = admin.firestore();
+
+const collectionName = 'tsia-complex-entities';
+const docId = '00ABWDQmABQWXPAem5Ya';
+
+async function checkListId() {
+  try {
+    const docRef = db.collection(collectionName).doc(docId);
+    const docSnap = await docRef.get();
+
+    if (docSnap.exists) {
+      console.log('Document data:', docSnap.data());
+    } else {
+      console.log('No document found!');
+    }
+  } catch (error) {
+    console.error('Error fetching document:', error);
+  }
+}
+
+checkListId();
