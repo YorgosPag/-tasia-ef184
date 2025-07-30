b2635602f25c06edfef34e0eb49427ada9cf82f0
Georgios Pagonis <georgios.pagonis@gmail.com>
2025-07-30 13:58:54 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,6 @@ const config = {
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/features/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   prefix: '',
   theme: {
