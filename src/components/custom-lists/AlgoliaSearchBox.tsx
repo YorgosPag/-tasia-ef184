131acf31f46b2be5a7fd5a8da8bc6bc7542d11a6
Georgios Pagonis <georgios.pagonis@gmail.com>
2025-07-31 13:13:42 +0000

    ✅ Formatting OK (prettier --check). 🚦 2. Type checking (tsc --noEmit)..

--- src/components/custom-lists/AlgoliaSearchBox.tsx
+++ src/components/custom-lists/AlgoliaSearchBox.tsx
@@ -156,9 +156,7 @@ export function AlgoliaSearchBox({
   return (
     <div className="w-full">
       <InstantSearch searchClient={searchClient} indexName={indexName}>
-        {listType && (
-          <Configure filters={`type:'${listType}'`} hitsPerPage={50} />
-        )}
+        {listType && <Configure filters={`type:'${listType}'`} hitsPerPage={50} />}
         <div className="grid grid-cols-1 gap-4">
           <SearchBox />
           <Hits onHitsChange={onHitsChange} />
