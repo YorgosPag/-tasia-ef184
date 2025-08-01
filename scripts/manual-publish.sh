6746b63d6d5151afb6e2475bdf5e624e3c0444e6
YorgosPag <georgios.pagonis@gmail.com>
2025-08-01 16:09:38 +0300

    Delete scripts directory

--- scripts/manual-publish.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Βήμα 22: Manual publish στο Firebase Studio
-
-set -e
-
-echo "🚦 Βήμα 22: Πήγαινε τώρα στο Firebase Studio και πάτα το μπλε κουμπί PUBLISH για να βγουν live οι αλλαγές σου!"
-echo "👉 https://console.firebase.google.com/project/ΤΟ_ΟΝΟΜΑ_ΤΟΥ_PROJECT_SOU/studio"
-echo ""
