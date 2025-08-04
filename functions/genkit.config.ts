ec7f8d924e9c1fe38b778fcca439e929beb44789
Georgios Pagonis <georgios.pagonis@gmail.com>
2025-08-04 11:03:34 +0000

    studio-3036910398:~/studio{main}$ npm run build
    > tasia@0.1.0 build
    > n

--- functions/genkit.config.ts
+++ functions/genkit.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from '@genkit-ai/core';
+import { genkit } from '@genkit-ai/core';
 import { googleAI } from '@genkit-ai/googleai';
 
-export default defineConfig({
-  plugins: [googleAI()],
+export default genkit({
+  plugins: [googleAI({
+    apiVersion: "v1beta"
+  })],
   flows: [],
+  logLevel: 'debug',
+  enableTracingAndMetrics: true,
 });
